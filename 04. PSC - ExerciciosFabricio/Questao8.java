5984499986791ee43810a58a4081f19febef3535
Giulia Huguinim <159452027+GiuliaHuguinim@users.noreply.github.com>
2024-03-31 21:04:14 -0300

    Add files via upload

---
+++ 04. PSC - ExerciciosFabricio/Questao8.java
@@ -0,0 +1,4 @@
+/*Escreva um programa em Java que calcule o produto de A (número real) por B (número inteiro), 
+ou seja, A*B, por intermédio de adições sucessivas. Tanto A quanto B devem ser fornecidos pela 
+pessoa utilizadora do programa.
+ */
