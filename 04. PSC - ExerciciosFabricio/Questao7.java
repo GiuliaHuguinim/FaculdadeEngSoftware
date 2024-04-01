da5e2298201c210bd28837fed66396030eb6fecb
Giulia Huguinim <159452027+GiuliaHuguinim@users.noreply.github.com>
2024-03-31 22:28:17 -0300

    Update Questao7.java

--- 04. PSC - ExerciciosFabricio/Questao7.java
+++ 04. PSC - ExerciciosFabricio/Questao7.java
@@ -1,6 +1,5 @@
 /*Crie um programa em Java que exiba formas geométricas na tela. Um menu deve ser exibido com as seguintes opções: retângulo, diagonal superior esquerda, diagonal superior direita, diagonal inferior esquerda, diagonal inferior direita. As imagens devem ser formadas por asteriscos (*) e, a pessoa utilizadora deve informar a quantidade de colunas.
 Abaixo estão alguns exemplos das formas:*/
-/* Crie um programa em Java que exiba formas geométricas na tela. Um menu deve ser exibido com as seguintes opções: retângulo, diagonal superior esquerda, diagonal superior direita, diagonal inferior esquerda, diagonal inferior direita. As imagens devem ser formadas por asteriscos (*) e, a pessoa utilizadora deve informar a quantidade de colunas. Abaixo estão alguns exemplos das formas: */
 
 import java.util.Scanner;
 
