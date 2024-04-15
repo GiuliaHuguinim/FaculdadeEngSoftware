a83b6101b9087da659f1047f3840d8e19e803e70
Giulia Huguinim <159452027+GiuliaHuguinim@users.noreply.github.com>
2024-04-15 20:41:57 -0300

    Update Questao1.java

--- 06. PSC - Exercícios Fabrício/Questao1.java
+++ 06. PSC - Exercícios Fabrício/Questao1.java
@@ -1,7 +1,7 @@
-/*Faça um programa que preencha um vetor com nove números inteiros,
- calcule e mostre os números primos e suas respectivas posições.*/
+ /*Faça um programa que preencha um vetor com nove números inteiros,
+calcule e mostre os números primos e suas respectivas posições.*/
  
- import java.util.Scanner;
+import java.util.Scanner;
 
 public class Questao1 {
 
@@ -9,6 +9,19 @@ public class Questao1 {
 
         Scanner dadosScanner = new Scanner(System.in);
         
+        int[] vetor = new int[9];
 
+        for(int i = 0; i < vetor; i++){
+            System.out.println("Digite 9 números inteiros: ");
+            int numero=dadosScanner.nextInt();
+
+            vetor[i] = numero;
+        }
+
+        for(int i = 0; i < vetor; i++){
+            System.out.println(vetor[i]);
+        }
+
+        dadosScanner.close();
     }
 }
