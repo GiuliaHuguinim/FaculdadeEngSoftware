704014f83fad9e386cd779be47b85e65171618c2
Giulia Huguinim <159452027+GiuliaHuguinim@users.noreply.github.com>
2024-04-15 19:54:22 -0300

    Add files via upload

---
+++ 06. PSC - Exercícios Fabrício/06. PSC - Exercícios Fabrício/Questao3.java
@@ -0,0 +1,11 @@
+/*Faça um programa que preencha um vetor com oito números inteiros, calcule e mostre dois vetores 
+resultantes. O primeiro vetor resultante deve conter os números positivos e o segundo, os números negativos.
+ Cada vetor resultante vai ter, no máximo, oito posições, que não poderão ser completamente utilizadas.*/
+import java.util.Scanner;
+
+public class Questao3 {
+
+    public static void main(String[] args) {
+
+        Scanner dadosScanner = new Scanner(System.in);
+        
