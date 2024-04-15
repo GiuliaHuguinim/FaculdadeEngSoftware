becaeb08d815cf599022eb5abfdb576af4913e88
Giulia Huguinim <159452027+GiuliaHuguinim@users.noreply.github.com>
2024-04-15 19:55:20 -0300

    Add files via upload

---
+++ 06. PSC - Exercícios Fabrício/Questao1.java
@@ -0,0 +1,14 @@
+/*Faça um programa que preencha um vetor com nove números inteiros,
+ calcule e mostre os números primos e suas respectivas posições.*/
+ 
+ import java.util.Scanner;
+
+public class Questao1 {
+
+    public static void main(String[] args) {
+
+        Scanner dadosScanner = new Scanner(System.in);
+        
+
+    }
+}
---
+++ 06. PSC - Exercícios Fabrício/Questao2.java
@@ -0,0 +1,12 @@
+/*Faça um programa que preencha dois vetores de dez elementos numéricos cada um e
+ mostre o vetor resultante da intercalação deles.*/
+
+import java.util.Scanner;
+
+public class Questao2 {
+
+    public static void main(String[] args) {
+
+        Scanner dadosScanner = new Scanner(System.in);
+    }
+}
