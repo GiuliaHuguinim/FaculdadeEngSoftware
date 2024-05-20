d13b0471dc6aae717bd1e9ae40c5de387a0f7398
Giulia Huguinim <159452027+GiuliaHuguinim@users.noreply.github.com>
2024-05-20 20:03:28 -0300

    Update Empregado.java

--- 08. PSC - Exercícios Fabrício/Empregado.java
+++ 08. PSC - Exercícios Fabrício/Empregado.java
@@ -4,36 +4,84 @@ public class Empregado {
     private String nome;
     private int idade;
     private double salario;
+    
+        public Empregado(String nome, int idade, double salario) {
+            this.nome = nome;
+            this.idade = idade;
+            this.salario = salario;
+        }
+    
+        // Métodos getters e setters
+        public String getNome() {
+            return nome;
+        }
+    
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+    
+        public int getIdade() {
+            return idade;
+        }
+    
+        public void setIdade(int idade) {
+            this.idade = idade;
+        }
+    
+        public double getSalario() {
+            return salario;
+        }
+    
+        public void setSalario(double salario) {
+            this.salario = salario;
+        }
 
-    public static void main(String[] args){
-        Scanner dadosScanner = new Scanner(System.in);
-        Empregado empregado = new Empregado();
-
-        System.out.println("Digite seu nome: ");
-        empregado.nome = dadosScanner.nextLine();
-
-        System.out.println("Digite sua idade: ");
-        empregado.idade = dadosScanner.nextInt();
-
-        System.out.println("Digite seu salario: ");
-        empregado.salario =dadosScanner.nextDouble();
-
-        promover();
+public String toString() {
+    return "Empregado{" +
+            "nome='" + nome + '\'' +
+            ", idade=" + idade +
+            ", salario=" + salario +
+            '}';
+}
+public void promover() {
+    if (this.idade > 18) {
+        aumentarSalario(25);
     }
+}
 
-    public static void promover(<Class> empregado){
-        
-    }
+public void aumentarSalario(double percentual) {
+    this.salario += this.salario * (percentual / 100);
+}
 
-    public static void aumentarSalario(String[]args){
-        
-    }
+public void demitir(int motivo) {
+    switch (motivo) {
+        case 1:
+            System.out.println("Demitido por justa causa. Deve cumprir aviso prévio.");
+            break;
 
-    public static void demitir(String[]args){
-        
-    }
+        case 2:
+            double multa = this.salario * 0.40;
+            System.out.println("Demitido por decisão do empregador. Multa de 40%: " + multa);
+            break;
 
-    public static void felizAniversario(String[]args){
-        
+        case 3:
+            if (this.salario >= 1000 && this.salario < 2000) {
+                this.salario = 1500;
+            } else if (this.salario >= 2000 && this.salario < 3000) {
+                this.salario = 2500;
+            } else if (this.salario >= 3000 && this.salario < 4000) {
+                this.salario = 3500;
+            } else if (this.salario >= 4000) {
+                this.salario = 4000;
+            }
+            System.out.println("Aposentado com salário de: " + this.salario);
+            break;
+
+        default:
+            System.out.println("Motivo inválido.");
     }
+}
+
+public void fazerAniversario() {
+    this.idade += 1;
 }
